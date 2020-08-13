418d83d8295fb00e66689d2e187b0d280f1749ff
LSVG <laura2162144@correo.uis.edu.co>
2020-08-12 20:28:22 -0500

    registro ya

--- validacion.js
+++ validacion.js
@@ -3,7 +3,7 @@ const disableds = Array.from(document.querySelectorAll('.disabled'));
 const selectDptos = document.getElementById('departamentos');
 const selectMun = document.getElementById('municipios');
 
-$(document).ready(function () {
+paises.addEventListener('change', (e) => {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://www.datos.gov.co/resource/xdk5-pm3f.json');
 
@@ -60,8 +60,7 @@ $(document).ready(function () {
         });
     });
     xhr.send();
-$('select').formSelect();
-});
+})
 
 var ccpaswd = document.getElementById('ccpaswd');
 var ccpaswdValidate = document.getElementById('ccpaswdValidate');
